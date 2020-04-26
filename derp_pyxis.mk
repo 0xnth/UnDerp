a7a9d30ad5bb9fada3f8017336c0f45cb7b5a594
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-27 03:27:38 +0700

    pyxis: Miui ANX Camera
    Signed-off-by: mrhainavn <mr.hai.na.vn@gmail.com>

--- derp_pyxis.mk
+++ derp_pyxis.mk
@@ -24,3 +24,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="pyxis"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Miui Camera Mod AOSP Custom
+$(call inherit-product, vendor/MiuiCamera/config.mk)
