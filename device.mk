7b29a3eec29e4467eac75f320f433bdfeb69ada5
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2020-05-02 13:48:24 +0000

    pyxis: Enable FOD
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- device.mk
+++ device.mk
@@ -179,7 +179,6 @@ PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
-    vendor/aosip/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml \
     $(LOCAL_PATH)/configs/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_pyxis.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_pyxis.rc
 
 # Net
